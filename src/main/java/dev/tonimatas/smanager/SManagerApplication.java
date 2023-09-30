2d8e379341ccfe86758faea9eaff7312049e43aa
TonimatasDEV <amatascubero@gmail.com>
2023-09-30 12:39:43 +0200

    Initial commit

---
+++ src/main/java/dev/tonimatas/smanager/SManagerApplication.java
@@ -0,0 +1,13 @@
+package dev.tonimatas.smanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/tonimatas/smanager/SManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tonimatas.smanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
