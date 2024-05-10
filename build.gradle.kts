f1d07f45b90e468e13d65636f365c5456721be66
TonimatasDEV <amatascubero@gmail.com>
2024-05-10 18:31:45 +0200

    Update to Java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,31 +1,25 @@
 plugins {
-    java
-    war
-    id("org.springframework.boot") version "3.1.4"
-    id("io.spring.dependency-management") version "1.1.3"
+	java
+	id("org.springframework.boot") version "3.2.5"
+	id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "dev.tonimatas"
-version = "0.0.1-SNAPSHOT"
-
-base {
-    archivesName.set("Api")
-}
+version = "0.0.1"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
+	sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
